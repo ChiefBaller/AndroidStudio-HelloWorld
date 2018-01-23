ae5a4585604742510011e3fb3a9d03f4cd8f8aa9
KingKondak <alvarezzane@gmail.com>
2018-01-23 15:15:24 -0600

    Second Commit and confirmation of IntelliJ Git memory

--- app/src/main/java/com/example/zane/helloworld/MainActivity.java
+++ app/src/main/java/com/example/zane/helloworld/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("MainActivity", "Hello World");
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
     }
 }
